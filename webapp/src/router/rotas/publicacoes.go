553d64c81a521db1a753af55b937a5880aee969b
vinicio schiavo <vinicioschiavo@gmail.com>
2023-03-10 18:35:55 -0300

    add methodDelete para as publicacoes

--- webapp/src/router/rotas/publicacoes.go
+++ webapp/src/router/rotas/publicacoes.go
@@ -37,4 +37,10 @@ var rotasPublicacoes = []Rota{
 		Funcao:             controllers.AtualizarPublicacao,
 		RequerAutenticacao: true,
 	},
+	{
+		URI:                "/publicacoes/{publicacaoId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarPublicacao,
+		RequerAutenticacao: true,
+	},
 }
